12f9a18f3864eb0c3dced2bccf0f009e0260f3d8
rodrii-rms <35659055+rodrii-rms@users.noreply.github.com>
2018-06-24 21:05:33 -0300

    FIX
    fix error attempt to compare number with table and we avoid possible xPlayer errors with _source

--- server.lua
+++ server.lua
@@ -4,17 +4,19 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('esx_slotmachine:sv:1')
 AddEventHandler('esx_slotmachine:sv:1', function(amount,a,b,c)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	amount = tonumber(amount)
-		if (xPlayer.getMoney() >= tonumber(amount)) or (xPlayer.getAccounts('black_money') >= tonumber(amount)) then
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local amount = tonumber(amount)
+	if not (xPlayer.getMoney() >= tonumber(amount)) or (xPlayer.getAccounts('black_money') >= tonumber(amount)) then
+		TriggerClientEvent('esx:showNotification', source, "Not enough money in your wallet")
+	else
 		xPlayer.removeMoney(amount)
 		-- xPlayer.removeAccountMoney('black_money', (amount))
-		TriggerClientEvent("esx_slotmachine:1", source, tonumber(amount), tostring(a), tostring(b), tostring(c))
-	else
-		TriggerClientEvent('esx:showNotification', source, "Not enough money in your wallet")
+		TriggerClientEvent("esx_slotmachine:1", _source, tonumber(amount), tostring(a), tostring(b), tostring(c))
 	end
 end)
+
+
 
 RegisterServerEvent('esx_slotmachine:sv:2')
 AddEventHandler('esx_slotmachine:sv:2', function(amount)
@@ -23,4 +25,4 @@ AddEventHandler('esx_slotmachine:sv:2', function(amount)
 	    xPlayer.addMoney(amount)
 	    -- xPlayer.addAccountMoney('black_money', (amount))
 	    TriggerClientEvent('esx:showNotification', source, "You WON : " .. amount .. "$")
-	end)
+end)
